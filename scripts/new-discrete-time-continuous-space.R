1e1e0188f1020a1ae58d934c7bd6b582761d2f0d
ikk23 <ikk23@cornell.edu>
2022-02-28 11:13:33 -0500

    update. now need to figure out the integration again

--- scripts/new-discrete-time-continuous-space.R
+++ scripts/new-discrete-time-continuous-space.R
@@ -7,7 +7,7 @@ library(gganimate)
 library(magick)
 require(pracma)
 
-# Start with individuals only released in a square with width a and height b
+# Function to simulate an initial square with width (a) and height (b)
 u_initial = function(x,a,b){
   if (x >= -a/2 & x <= a/2){
     u = b
@@ -17,62 +17,29 @@ u_initial = function(x,a,b){
   return(u)
 }
 
-# ex: a = 4 and b = 4. Therefore, the area is bounded by x = -2 and x = 2.
-a=6
-b=0.3 # frequency of 30%
-k = 0.1
-u_hat = 0.1
-x_grid = seq(-10,10,by=0.01)
-u_x_t0 = rep(4,length(x_grid))
-for (i in 1:length(x_grid)){
-  u_x_t0[i] = u_initial(x_grid[i],a,b)
-}
-plot(x_grid,u_x_t0)
-
-# The dispersal kernel is the exponential, with mean = sigma. This means that the rate is 1/sigma.
-sigma = 0.5
-rate = 1/sigma
-x = 0
-x_prime = 2
-x_distance = abs(x-x_prime)
-dispersal_probability = dexp(x=x_distance,rate=rate)
-
+# Function for the probability of dispersing a distance of |x-x_prime| based on 
+# the exponential distribution with mean of sigma / rate of 1/sigma.
 exponential_dispersal_kernel = function(sigma,x,x_prime){
-  # sigma = the average dispersal distance
   rate = 1/sigma
   x_distance = abs(x-x_prime)
   p = dexp(x=x_distance,rate=rate)
   return(p)
 }
 
-# Plot some of this kernel
-x_distances = seq(0,10,by=0.1)
-sigma=0.5
-rate = 1/sigma
-probs = dexp(x_distances,rate=rate)
-plot(x_distances,probs)
-
-# u'(x,0) = integral of (b)(exponential_dispersal_kernel) for t=0
-u_prime_of_x_t0 = function(x,a,b,sigma){
-  
-  if (x < (-a/2) || x > (a/2)){
+# Function to get the new u'(x,t) curve after migration but before the reaction equation
+u_prime_x = function(u_x_here, x, sigma, a){
+  # when t = 0, u_x_here is always b
+  # when t > 0, u_x_here = u(x') = u(x) at this new position
+  if (x < -a/2 || x > a/2){
     return(0)
   }
-  
-  e_term_1 = exp((-1/sigma)*(x+(a/2)))
-  e_term_2 = exp((-1/sigma)*((a/2)-x))
-  sol = b*(2-e_term_1-e_term_2)
+  e_term_1 = exp((-1/sigma)*(x + (a/2)))
+  e_term_2 = exp((-1/sigma)*((a/2) - x))
+  sol = u_x_here*(2-e_term_1-e_term_2)
   return(sol)
 }
 
-n = length(u_x_t0)
-u_prime_t0 = rep(-1,n)
-for (i in 1:n){
-  x = x_grid[i]
-  u_prime_t0[i] = u_prime_of_x_t0(x,a,b,sigma)
-}
-
-# Reaction
+# Barton 2011's bistable wave reaction equation. Apply this to u'(x,t) after migration
 bistable_reaction_function = function(u,k,u_hat){
   f = 2*k*u*(1-u)*(u-u_hat)
   return(f)
@@ -84,21 +51,103 @@ for (i in 1:n){
   u_x_t1[i] = bistable_reaction_function(u_prime_t0[i],k,u_hat)
 }
 
-library(tidyverse)
-library(ggplot2)
-info = tibble(x = x_grid, u_x_t0 = u_x_t0, u_prime_t0 = u_prime_t0, u_x_t1 = u_x_t1)
+simulate = function(x_grid, a, b, sigma, k, u_hat,time_steps){
+  n = length(x_grid)
+  lower = -a/2
+  index_lower = which(x_grid == lower)
+  upper = a/2
+  index_upper = which(x_grid == upper)
+  
+  if (x_grid[1] > lower || x_grid[n] < upper){
+    stop("x_grid has to be bigger")
+  }
+  if (length(index_lower) == 0 || length(index_upper) == 0){
+    stop("make sure -a/2 and a/2 are within x_grid")
+  }
+  
+  # Vectors to store all the simulation data in for the gganimation 
+  x_expanded_grid = c() # length = time_steps*n
+  time_expanded = c() # length = time_steps*n
+  u_x_expanded = c() # length = time_steps*n
+  AUC_with_time = c() # only 1 per timestep, so length = time_steps
+  time_vector = c() # simply 0,1,...,timesteps
+  
+  # Set up u(x,t=0) based on the initial square function
+  u_x_t = rep(-1,n)
+  for (i in 1:n){
+    x = x_grid[i]
+    u_x_t[i] = u_initial(x,a,b)
+  }
+  # Store u(x,t=0) data
+  x_expanded_grid = c(x_expanded_grid, x_grid) 
+  time_expanded = c(time_expanded, rep(0,n))
+  u_x_expanded = c(u_x_expanded, u_x_t) 
+  
+  time_vector = c(time_vector, 0)
+  AUC_with_time = c(AUC_with_time, a*b)
+  
+  # moving beyond t=0:
+  for (t in 1:time_steps){ # t = the time step we're aiming to find u(x,t) for. Currently have u(x,t-1).
+    u_x_prime_t_vec = rep(-1,n)
+    u_x_t_new = rep(-1,n)
+    # Loop through the x-grid and, if x is within [-a/2, a/2], apply the migration function and then reaction function
+    for (i in 1:n){
+      x = x_grid[i]
+      
+      # If x is out of range, don't bother applying the migration or reaction functions
+      if (x < lower || x > upper){
+        u_x_prime_t_vec[i] = 0
+        u_x_t_new[i] = 0
+      } else {
+        constant = u_x_t[i] # u(x',t) = u(x,t) after moving from x to x' here....
+        u_x_prime_t_vec[i] = u_prime_x(constant,x,sigma,a) # curve after migration -- NEED TO FIX.
+        u_x_t_new[i] = bistable_reaction_function(u_x_prime_t_vec[i], k, u_hat)
+      }
+    }
+    # Calculate new AUC
+    AUC = trapz(x=x_grid[index_lower:index_upper],y=u_x_t_new[index_lower:index_upper])
+      
+    # Save variables
+    x_expanded_grid = c(x_expanded_grid, x_grid) 
+    time_expanded = c(time_expanded, rep(t,n))
+    u_x_expanded = c(u_x_expanded, u_x_t_new) 
+    time_vector = c(time_vector,t)
+    AUC_with_time = c(AUC_with_time, AUC)
+    
+    # Save new u_x_t
+    u_x_t = u_x_t_new
+  }
+  
+  concacenated_results = tibble(x=x_expanded_grid,
+                                u_x = u_x_expanded,
+                                t = time_expanded)
+  
+  y_upper = max(concacenated_results$u_x)
+  y_lower = min(concacenated_results$u_x)
+  
+  plot = ggplot(data = concacenated_results, aes(x = x, y = u_x)) + 
+    geom_point(color="red",aes(group=t)) + 
+    transition_time(t) +
+    labs(x = "x", y = "u(x,t)", title = 't: {frame_time}') +
+    xlim(x_grid[1], x_grid[n]) + ylim(y_lower,y_upper) + 
+    ylab("u(x)") + 
+    xlab("x")
+  
+  print(plot)
+  
+  return(list(concacenated_results = concacenated_results, time_vector = time_vector, AUC_vector = AUC_with_time))
+  
+}
 
-plot = ggplot(info) + 
-  geom_point(aes(x=x,y=u_x_t0),col="black") + 
-  geom_point(aes(x=x,y=u_prime_t0),col="yellow") + 
-  geom_point(aes(x=x,y=u_x_t1),col="red") +
-  geom_hline(yintercept=0) + geom_vline(xintercept = 0) +
-  xlim(-2,2) + ylim(0,0.6)
-plot
 
-AUC = trapz(x = x_grid[c(which(x_grid==-2):which(x_grid==2))], y = u_x_t1[c(which(x_grid==-2):which(x_grid==2))])
-AUC > (a*b) # FALSE
-
+x_grid = seq(-3,3,by=0.01)
+a = 4
+b = 0.4
+u_hat = 0.2
+k = 0.05
+time_steps = 3
+sigma = 0.5
+r = simulate(x_grid,a,b,sigma,k,u_hat,time_steps)
 
 time_step1 = function(x_grid,a,b,sigma,k,u_hat,plot=TRUE){
   n = length(x_grid)
@@ -174,27 +223,6 @@ time_steps_after_1 = function(u_x,x_grid,a,b,sigma,k,u_hat){
   
   return(u_x_next)
 }
-
-a = 4
-b = 0.3
-u_hat = 0.01
-k = 0.99
-sigma = 0.2
-x_grid = seq(-a/2,a/2,by=0.01)
-res = time_step1(x_grid,a,b,sigma,k,u_hat)
-res$ab
-res$AUC_u_x_t1
-res$auc_increased
-
-res_after_t2 = time_steps_after_1(res$u_info$u_x_t1, x_grid,a,b,sigma,k,u_hat)
-info_more = res$u_info %>% add_column(u_x_t2 = res_after_t2)
-plt = ggplot(info_more) + 
-      geom_point(aes(x=x,y=u_x_t0),col="black") + 
-      geom_point(aes(x=x,y=u_x_t1),col="red") +
-      geom_point(aes(x=x,y=u_x_t2), col="purple")+
-      geom_hline(yintercept=0) + geom_vline(xintercept = 0) +
-      xlim(-(a/2),(a/2)) + ylim(0,1.0) + ylab("u(x)")
-plt
 
 simulate_discrete_time_wave = function(x_grid,a,b,sigma,k,u_hat,time_steps){
   step1 = time_step1(x_grid,a,b,sigma,k,u_hat,plot=FALSE)
