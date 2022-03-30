2c0297228f91371270a2ab66dbf1fb1d0fcc4510
ikk23 <ikk23@cornell.edu>
2022-03-30 12:07:02 -0400

    Done with retrieving cluster output

--- cluster/create_slurm_text_file.R
+++ cluster/create_slurm_text_file.R
@@ -1,14 +1,15 @@
-a_seq = seq(0.01, 0.03, length.out = 100)
+a_seq = seq(0.001, 0.0049, length.out = 20)
+u_hat = 0.05
 
 # Start writing to a new output file
-sink('/Users/isabelkim/Desktop/year2/underdominance/reaction-diffusion/cluster/u_hat=0.2_run/slurm_text/u20_a_0.01_to_0.03_50_replicates.txt')
+sink('/Users/isabelkim/Desktop/year2/underdominance/reaction-diffusion/cluster/u_hat=0.05_run/slurm_text/u5_lower_a.txt')
 
 for (i in 1:length(a_seq)){
   a = a_seq[i]
   if (i == 1){
-    line = paste0("python python_driver.py -a ",a," -u_hat 0.2 -header","\n")
+    line = paste0("python python_driver.py -a ",a," -u_hat ",u_hat," -header","\n")
   } else {
-    line = paste0("python python_driver.py -a ",a, " -u_hat 0.2", "\n")
+    line = paste0("python python_driver.py -a ",a, " -u_hat ", u_hat, "\n")
   }
   cat(line)
 }
---
+++ cluster/u_hat=0.05_run/slurm_main/u5_a_lower.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+#SBATCH --ntasks=1
+#SBATCH --mem=1000
+#SBATCH --partition=short
+#SBATCH --job-name=u5_a_0.001_to_0.0049
+#SBATCH --output=u5_a_0.001_to_0.0049.txt
+#SBATCH --array=1-20
+
+# Run 100 array jobs, each with 10 replicates
+
+# Create and move to working directory for job:
+WORKDIR=/workdir/$USER/$SLURM_JOB_ID-$SLURM_ARRAY_TASK_ID
+mkdir -p $WORKDIR
+cd $WORKDIR
+
+# Copy files over to working directory:
+# Need the python driver, the slim model, the slimutil script, and the text file with command line arguments
+BASE_DIR=/home/ikk23
+cp $BASE_DIR/underdom/main_scripts/python_driver.py .
+cp $BASE_DIR/underdom/main_scripts/nonWF-model.slim .
+cp $BASE_DIR/underdom/main_scripts/slimutil.py .
+cp $BASE_DIR/underdom/text_files/u5_lower_a.txt .
+
+# Include SLiM in the path
+PATH=$PATH:/home/ikk23/SLiM/SLiM_build
+export PATH
+
+# Only the first .part file has the header
+# Output all files into an output-specific folder
+prog=`sed -n "${SLURM_ARRAY_TASK_ID}p" u5_lower_a.txt`
+$prog > ${SLURM_ARRAY_TASK_ID}.part
+cp ${SLURM_ARRAY_TASK_ID}.part $BASE_DIR/underdom/out_u5
+
+# Will need to merge later
+
+# Clean up working directory:
+rm -r $WORKDIR
