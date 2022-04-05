a84bb5b65fafd75f5b42286d93467ea9348575c0
ikk23 <ikk23@cornell.edu>
2022-04-05 13:43:24 -0400

    compare

--- cluster/plotting_functions.R
+++ cluster/plotting_functions.R
@@ -2,7 +2,7 @@ get_a_pred_and_a_obs = function(data){
   # data is a summary file with 8 columns (add the correct delta): a, sigma, beta, k, u_hat, delta, p_increase,delta_correct
   
   # get the row number of the lowest value of delta_correct use this to find a_pred and delta_pred
-  ind_pred = which.min(data$delta_correct)
+  ind_pred = which.min(abs(data$delta_correct))
   min_row = data[ind_pred,] 
   a_prop = min_row$a
   delta_min = min_row$delta_correct
